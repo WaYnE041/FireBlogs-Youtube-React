5bfa7c1844c4690896608d69097e1d7521fdc293
Wayne Orego <wayneekeh@gmail.com>
2023-08-04 19:05:22 -0400

    added auth comments to route guard

--- src/routes/GuardedRoutes.tsx
+++ src/routes/GuardedRoutes.tsx
@@ -5,12 +5,12 @@ function GuardedRoutes( { isRouteAccessible, redirectRoute = "/" }: { isRouteAcc
 
   const { isAuth, isAdmin } = useAuth()
   if(isAuth() !== undefined && isAdmin() !== undefined){
-    console.log("isAuth is " + isAuth())
-    console.log("isAdmin is " + isAdmin())
-    console.log("isSignOut is " + (!isAuth() && !isAdmin()))
+    console.log("isAuth is " + isAuth() + "\n" + 
+                "isAdmin is " + isAdmin() + "\n" + 
+                "isSignOut is " + (!isAuth() && !isAdmin()))
   }
-
   console.log("isroute accessible: " + isRouteAccessible)
+  
   return (
     isRouteAccessible ? <Outlet/> : <Navigate to={redirectRoute}/>
   )
