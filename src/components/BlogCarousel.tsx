120b0203df3896488c884100510a97b91e57eb2f
Wayne Orego <wayneekeh@gmail.com>
2024-10-20 19:17:56 -0400

    Display Title for Carousel

--- src/components/BlogCarousel.tsx
+++ src/components/BlogCarousel.tsx
@@ -21,29 +21,33 @@ function BlogPost({ posts, welcomeScreen }: {
 
 	return (
 		<div className="carousel">
-		<div id="slider">
-			{posts.map((post, index) => {
-				return (
-					<>
-						{index === 2 ?
-							<input type="radio" name="slider" id={"s" + (index + 1)}></input>
-							:
-							<input type="radio" name="slider" id={"s" + (index + 1)}></input>
-						}
-					</>
-				)
-			})}
-			{posts.map((post, index) => {
-				return (
-					<label htmlFor={"s" + (index+1)} id={"slide" + (index+1)}>
-						<img src={post.blogCoverPhoto} alt="Blog Cover Photo" />
-						<Link className="btn" to={`/view-blog/${post.blogID}`} >
-										View The Post<Arrow className="arrow" />
-									</Link>
-						</label>
-				)
-			})}
-		</div>
+			<h2></h2>
+			<div id="slider">
+				{posts.map((post, index) => {
+					return (
+						<><input type="radio" name="slider" id={"s" + (index + 1)}></input>
+						</>
+					)
+				})}
+				{posts.map((post, index) => {
+					return (
+						<>
+							<h2 id={"title" + (index+1)}>{post.blogTitle}</h2>
+						</>
+					)
+				})}
+				{posts.map((post, index) => {
+					return (
+						<label htmlFor={"s" + (index+1)} id={"slide" + (index+1)}>
+							<img src={post.blogCoverPhoto} alt="Blog Cover Photo" />
+							<Link className="btn" to={`/view-blog/${post.blogID}`} >
+											View The Post<Arrow className="arrow" />
+										</Link>
+							</label>
+					)
+				})}
+			</div>
+
 		</div>
 	)
 }
