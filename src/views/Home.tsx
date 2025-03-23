39957fe904aa30c21a0c715457c4641966589d75
Wayne Orego <wayneekeh@gmail.com>
2025-03-23 11:18:06 -0400

    sweep of Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,5 @@
 import '../styles/Home.css';
-import { ReactComponent as Arrow } from '../assets/Icons/arrow-right-light.svg';
-import { useAuth } from '../contexts/UserContext';
 import { useEffect } from 'react';
-import { Link } from "react-router-dom";
-
 
 function Home( { children }: { children: React.JSX.Element }) {
 
@@ -14,22 +10,9 @@ function Home( { children }: { children: React.JSX.Element }) {
 		};
 	}, []);
 
-	const { isAuth } = useAuth();
-
 	return (
 		<div className='home'>
 			{ children }
-
-			{ !isAuth && (
-				<div className="updates">
-					<div className="container">
-						<h2>Never Miss A Post. Register For Your Free Account Today!</h2>
-						<Link className="router-button" to="/register">
-							Register for FireBlogs <Arrow className="arrow arrow-light" />
-						</Link>
-					</div>
-				</div>
-			)}
 		</div>
 	)
 }
